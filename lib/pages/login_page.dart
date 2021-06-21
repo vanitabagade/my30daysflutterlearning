bfe4df46ba55e82139fa34607fb731eb40f7fe46
Vanita <bagadevanita221@gmail.com>
2021-06-21 15:56:19 +0100

    day 6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,41 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:myfirstapp/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool chagedvalue = false;
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/login_image.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            'Welcome',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 16,
-              horizontal: 32,
+            SizedBox(
+              height: 20,
             ),
-            child: Column(
-              children: [
+            Text(
+              'Welcome $name',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16,
+                horizontal: 32,
+              ),
+              child: Column(children: [
                 TextFormField(
                   decoration: InputDecoration(
                     hintText: 'Enter Username',
                     labelText: 'Username',
                   ),
+                  onChanged: (value) {
+                    name = value;
+                    this.setState(() {});
+                  },
                 ),
                 TextFormField(
                   obscureText: true,
@@ -45,21 +57,53 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
                 SizedBox(
-                  height: 20,
+                  height: 40,
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    print('hi vanita');
+                InkWell(
+                  onTap: () async {
+                    chagedvalue = true;
+                    setState(() {});
+                    await Future.delayed(Duration(seconds: 1));
+                    Navigator.pushNamed(context, Myroutes.homeroute);
                   },
-                  child: Text(
-                    'Login',
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    height: 40,
+                    width: chagedvalue ? 40 : 150,
+                    alignment: Alignment.center,
+                    child: chagedvalue
+                        ? Icon(
+                            Icons.done,
+                            color: Colors.white,
+                          )
+                        : Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 13),
+                          ),
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        /* shape: chagedvalue
+                              ? BoxShape.circle
+                              : BoxShape.rectangle, */
+                        borderRadius: BorderRadius.circular(20)),
                   ),
-                  style: TextButton.styleFrom(),
                 )
-              ],
-            ),
-          )
-        ],
+                /* ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, Myroutes.homeroute);
+                    },
+                    child: Text(
+                      'Login',
+                    ),
+                    style: TextButton.styleFrom(minimumSize: Size(150, 35)),
+                  ) */
+              ]),
+            )
+          ],
+        ),
       ),
     );
   }
